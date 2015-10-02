3d2b1ef2486579c8e6797406b2a17bdac0e4a7cc
javier.santayana <javier.santayana@splendidunlimited.com>
2015-10-02 14:37:27 +0100

    Fix upper file git confusion

--- app/src/main/java/com/trips/mochilo/ui/activity/main/MainModule.java
+++ app/src/main/java/com/trips/mochilo/ui/activity/main/MainModule.java
@@ -22,6 +22,6 @@ public class MainModule {
     @Provides
     @ActivityScope
     public MainPresenter provideActivityPresenter(MainView view) {
-        return new MainPresenterImpl(view);
+        return new MainPresenteriMPL(view);
     }
 }
---
+++ app/src/main/java/com/trips/mochilo/ui/activity/main/MainPresenterI.java
@@ -0,0 +1,9 @@
+package com.trips.mochilo.ui.activity.main;
+
+public class MainPresenterI implements MainPresenter {
+    private final MainView mView;
+
+    public MainPresenterI(MainView view) {
+        mView = view;
+    }
+}
