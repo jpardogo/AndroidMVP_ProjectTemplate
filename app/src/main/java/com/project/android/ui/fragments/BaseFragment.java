a981d5039e98811f102139a582dff67f219689e9
jpardogo <jpardogo@gmail.com>
2016-09-04 22:46:40 +0100

    Rename Example fragment

--- app/src/main/java/com/project/android/ui/fragments/BaseFragment.java
+++ app/src/main/java/com/project/android/ui/fragments/BaseFragment.java
@@ -2,5 +2,5 @@ package com.project.android.ui.fragments;
 
 import android.support.v4.app.Fragment;
 
-public class ExampleFragment extends Fragment {
+public class BaseFragment extends Fragment {
 }
